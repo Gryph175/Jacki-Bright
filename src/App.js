c51dc8fb5fc1fa687e8a018328f5673b0c40419e
Griffin Sanford <griffin.sanford@gmail.com>
2021-05-26 14:06:43 -0400

    lil update

--- src/App.js
+++ src/App.js
@@ -33,29 +33,31 @@ function App(){
 const About = () => (
     <div className= 'Home'>
        <h1 className="banner">About Me</h1>
+        <div className="contents">
+            <img className="Jacki" src={Jacki} alt="Jacki Bright" />
 
-       <img className="Jacki" src={Jacki} alt="Jacki Bright" />
+            <p className="par1">Hello!  Welcome to my leadership portfolio.  
+            I am currently the Deputy Head at MacLachlan College in Oakville, 
+            Ontario.  MacLachlan was founded in 1978 and is a co-educational day school 
+            including grades PK -12 with a population of just under 350 students.
+            </p>
 
-        <p className="par1">Hello!  Welcome to my leadership portfolio.  
-           I am currently the Deputy Head at MacLachlan College in Oakville, 
-           Ontario.  MacLachlan was founded in 1978 and is a co-educational day school 
-           including grades PK -12 with a population of just under 350 students.
-        </p>
+            <p className="par2">While working through the creation of this portfolio, I realized that all of my leadership artifacts 
+                fell into 2 categories of the Ontario Leadership Framework, Building Relationships and Setting Direction. 
+                My first reflection of this had me thinking I was really deficient in being a true leader.  However, as I 
+                continued to reflect I came to realize that a good leader understands that they cannot be great at everything 
+                and there is a need to create a leadership team that compliments and supports one another.
+            </p>
 
-        <p className="par2">While working through the creation of this portfolio, I realized that all of my leadership artifacts 
-            fell into 2 categories of the Ontario Leadership Framework, Building Relationships and Setting Direction. 
-            My first reflection of this had me thinking I was really deficient in being a true leader.  However, as I 
-            continued to reflect I came to realize that a good leader understands that they cannot be great at everything 
-            and there is a need to create a leadership team that compliments and supports one another.
-        </p>
-
-        <p className="par1">As educators our ultimate goal is student achievement and well-being. As leaders in education we need to be 
-            constantly asking ourselves; How can I lead excellent teachers to enhance student achievement and well-being?  
-            My leadership portfolio shares the artifacts that I feel best reflect the progress I have made through the 
-            process of becoming the Deputy Head at MacLachlan College.  I first started teaching at MacLachlan in September 
-            1992.  Throughout those years I earned opportunities to be in various leadership roles. Each role giving me 
-            experiences that  played a part in developing my leadership skills.
-        </p>
+            <p className="par1">As educators our ultimate goal is student achievement and well-being. As leaders in education we need to be 
+                constantly asking ourselves; How can I lead excellent teachers to enhance student achievement and well-being?  
+                My leadership portfolio shares the artifacts that I feel best reflect the progress I have made through the 
+                process of becoming the Deputy Head at MacLachlan College.  I first started teaching at MacLachlan in September 
+                1992.  Throughout those years I earned opportunities to be in various leadership roles. Each role giving me 
+                experiences that  played a part in developing my leadership skills.
+            </p>
+        </div>   
+       
 
        
     </div>
