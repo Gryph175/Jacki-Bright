a466e57bc4d1e27ad30595c4ce5c26a473cb844f
Griffin Sanford <griffin.sanford@gmail.com>
2021-06-04 13:38:54 -0400

    update

--- src/Relations.js
+++ src/Relations.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Relations = () => {
     return(
         <div>
-            <h1 className="banner">Building RelationShips</h1>
+            <h1 className="banner">Building Relationships</h1>
 
             <h1 className='Title'>Supporting Staff Through...</h1>
             <div className='container'>
@@ -14,9 +14,27 @@ const Relations = () => {
                 <li className="dot">Development and Ongoing Implementation of MacLachlan's New Teacher Orientation Days</li>
                 <p className='par'>My first year as a teacher at Mac did not have any orientation or training prior to the start of the school year for new teachers to MacLachlan.  The following year along with a co-worker who also started with me we developed a two-day orientation program that I continue to deliver every August.</p>
                 <li className="dot">Encouraging Professional Development</li>
-                <p className='par'>present a budget to the Board for staff PD and encourage all staff to participate in continued PD to encourage new practices that match their interests and the school's goals.  Some of these have included PYP Workshops, Levels 1, 2 & 3, Cohort 21, Harvard's Project Zero and CIS Workshops.</p>
+                <p className='par'>I present a budget to the Board for staff PD and encourage all staff to participate in continued PD to encourage new practices that match their interests and the school's goals.  Some of these have included PYP Workshops, Levels 1, 2 & 3, Cohort 21, Harvard's Project Zero and CIS Workshops.</p>
                 <li className="dot">Meetings & Conferences</li>
                 <p className='par'>I meet with teachers in several different ways, staff, divisional, grade and committee meetings are scheduled throughout the school year and each of those schedules are set in August prior to the start of that school year.  Individual meetings are made as needed and my door is always open.</p>
+                </ul>
+            </div>
+
+            <h1 className='Title'>Modelling the school's values and practices Through...</h1>
+            <div className='container'>
+                <ul>
+                <li className="dot">Participating in School Events and School Life</li>
+                <p className='par'>I do not lead from my office.  I lead by example.  I start every day with morning door duty and greet every student by name as they enter the building.  I also end the day with traffic duty to say good-bye to students as they leave for the day.  I am highly visible in school events including Galas, Fun Fairs, Spirit Days and Student Staff sports.</p>
+                <li className="dot">Continued Love of Learning</li>
+                <p className='par'>Learning never ends and I continue to expand my knowledge.</p>
+                <p className='par'>AQ courses</p>
+                <p className='par'>IB Workshops/Conferences</p>
+                <p className='par'>2014 IB Conference of Americas Workshop Presenter in Chicago</p>
+                <p className='par'>CIS Workshops</p>
+                <p className='par'>Coaching 2019</p>
+                <p className='par'>Unconference 2018</p>
+
+                
                 </ul>
             </div>
             
