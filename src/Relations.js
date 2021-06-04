63b891b05142b086602b076b83fc6dce11e92c6d
Griffin Sanford <griffin.sanford@gmail.com>
2021-06-04 11:49:40 -0400

    relations

--- src/Relations.js
+++ src/Relations.js
@@ -1,71 +1,32 @@
 import './Relations.css'
-import React, {useState} from 'react';
-import {IconContext} from 'react-icons';
-import {FiPlus, FiMinus} from 'react-icons/fi';
-import {Data} from './RelationsData';
-import {Data2} from './RelationsData2';
+import React from 'react';
+
+
 
 const Relations = () => {
+    return(
+        <div>
+            <h1 className="banner">Building RelationShips</h1>
 
-const [clicked, setClicked] = useState(false)
-
-const toggle = index => {
-    if(clicked === index) {
-        return setClicked(null)
-    }
-
-    setClicked(index)
-}
-
-    return (
-        <IconContext.Provider value={{ color: 'rgb(147, 46, 241)', size: '25px'}}>
-            <div className="AccordionSection">
-                <div className="Container">
-                    <h1 className='staff'>Supporting Staff</h1>
-                    {Data.map((item, index) => {
-                        return (
-                            <>
-                            <div className="Wrap" onClick={() => toggle(index)} key={index}>
-                                <h1 className="title">{item.title}</h1>
-                                <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
-                            </div>
-
-                            {clicked === index ? (
-                                <div className="Dropdown">
-                                    <p>{item.contents}</p>
-                                </div>
-                            ) : null}
-                            
-                                 
-                            </>
-                        )
-                    })}
-
-                    <h1 className='staff'>Modelling the school's values and practices</h1>
-                    {Data2.map((item, index) => {
-                        return (
-                            <>
-                            <div className="Wrap" onClick={() => toggle(index)} key={index}>
-                                <h1 className="title">{item.title}</h1>
-                                <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
-                            </div>
-
-                            {clicked === index ? (
-                                <div className="Dropdown">
-                                    <p>{item.contents}</p>
-                                </div>
-                            ) : null}
-                            
-                                 
-                            </>
-                        )
-                    })}
-                </div>
-                
+            <h1 className='Title'>Supporting Staff Through...</h1>
+            <div className='container'>
+                <ul>
+                <li className="dot">Development and Ongoing Implementation of MacLachlan's New Teacher Orientation Days</li>
+                <p className='par'>My first year as a teacher at Mac did not have any orientation or training prior to the start of the school year for new teachers to MacLachlan.  The following year along with a co-worker who also started with me we developed a two-day orientation program that I continue to deliver every August.</p>
+                <li className="dot">Encouraging Professional Development</li>
+                <p className='par'>present a budget to the Board for staff PD and encourage all staff to participate in continued PD to encourage new practices that match their interests and the school's goals.  Some of these have included PYP Workshops, Levels 1, 2 & 3, Cohort 21, Harvard's Project Zero and CIS Workshops.</p>
+                <li className="dot">Meetings & Conferences</li>
+                <p className='par'>I meet with teachers in several different ways, staff, divisional, grade and committee meetings are scheduled throughout the school year and each of those schedules are set in August prior to the start of that school year.  Individual meetings are made as needed and my door is always open.</p>
+                </ul>
             </div>
             
-        </IconContext.Provider>
+
+        </div>
+        
+
+
     );
+
 };
 
 export default Relations;
