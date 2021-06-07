3bcfe41ca899b423eaba31a20980ba78494a6d75
Griffin Sanford <griffin.sanford@gmail.com>
2021-06-07 12:58:28 -0400

    developing update

--- src/Accountability.js
+++ src/Accountability.js
@@ -9,8 +9,8 @@ function Accountability(){
             <h1 className='Title'>Accountability to External Bodies</h1>
             <div className='container'>
                 <ul>
-                <li className="dot">I have experience leading staff through an evaluation process from an external body.  It is important to show that we are doing what we say we are doing.  Being held accountable gives our families the reassurance that we are who we say we are.  It also holds ourselves accountable to continue to offer a program that provides our students with rich learning engagements.  </li>
-                <li className="dot">I have led our school through a PYP authorization visit in 2010, evaluation visit in 2015 and a Ministry inspection every other year since 2013.  An important piece of the evaluation process is openly sharing with staff the results of the reports  Reflecting on feedback often drives changes in curriculum delivery and improved student achievement.  Feedback from external bodies can also drive future professional development plans.</li>
+                <li className="par">I have experience leading staff through an evaluation process from an external body.  It is important to show that we are doing what we say we are doing.  Being held accountable gives our families the reassurance that we are who we say we are.  It also holds ourselves accountable to continue to offer a program that provides our students with rich learning engagements.  </li>
+                <li className="par">I have led our school through a PYP authorization visit in 2010, evaluation visit in 2015 and a Ministry inspection every other year since 2013.  An important piece of the evaluation process is openly sharing with staff the results of the reports  Reflecting on feedback often drives changes in curriculum delivery and improved student achievement.  Feedback from external bodies can also drive future professional development plans.</li>
                 </ul>
             </div>
         </div>
--- src/Develop.js
+++ src/Develop.js
@@ -5,6 +5,34 @@ function Develop(){
     return(
         <div className='Develop'> 
             <h1 className="banner">Developing the Organization</h1>
+
+            <h1 className='Title'>Building Collaborative Cultures</h1>
+            <div className='container'>
+                <ul>
+                    <li className="dot">Assemblies</li>
+                    <p className='par'>I organize assemblies to share and celebrate student and staff successes.  Included in every assembly are class presentations. Student action, school updates and a mindful minute.  Parents are welcome to all of our assemblies.</p>
+                    <li className="dot">Policies & Schedules keep expectations clear and allow time for collaboration during the school day</li>
+                    <p className='par'>Along with the Head of School annual review and update of the Staff Handbook <br/>
+                        Creation of the Language, Assessment and PYP Policies <br />
+                        Yard Duty Policy and Schedule - create schedule to allow for common grade partner duties to allow for collaboration during the school day <br />
+                        Assembly Policy and schedule <br />
+                        Field Trip Policies and Co-ordination of field trips - divisional trips for a full day</p>
+                </ul>
+            </div>
+
+             <h1 className='Title'>Connecting the school to the wider community</h1>
+            <div className='container'>
+                <ul>
+                    <li className="dot">Kerr Street Mission</li>
+                    <p className='par'>for many years the school has organized food drives throughout the year and a staff member delivers the goods collected until a student asked my a question.  "Ms. Bright, what happens to all the things we bring in to collect house points?".  That sparked me to develop a relationship between the students at Mac and the Kerr Street Mission, where we bring the collected goods.  Now during one week in the school year, a homeroom class collects goods that the Mission needs at that time and the students deliver these goods.  They have a tour of the facility and listen to the reasons why people in the community need their help and how their goods are used. Students sort their goods and help organize the store before their visit is over.  For most students it is their first introduction ways in which they can volunteer.</p>
+                    <li className="dot">Trafalgar Lodge</li>
+                    <p className='par'>A senior citizens home down the street from the school.  Planning themed days for students to walk down to the home and sing for the seniors and then spend time either one on one or in small groups talking with them.  Students come prepared with questions to ask and the seniors love to have someone to tell their story to.  It's a win-win!</p>
+                    <li className="dot">Kawenni:io/Gaweni:yo School</li>
+                    <p className='par'>Kawenni:io/Gaweni:yo School is a private Mohawk and Cayuga immersion school that I connected with and since 2017 visit their school with a group of grade 5 students from MacLachlan and the following year a group of students from Kawenni:io/Gaweni:yo visit Mac. Students share with one another their own culture and it's beliefs and traditions.</p>
+                    <li className="dot">IB Conference of Americas</li>
+                    <p className='par'>Attending the annual IB Conference of Americas has been valuable PD and I learned a great deal through sharing of best practices.  In 2015 I was a presenter at the IB Conference of Americas in Chicago presenting on Active Learning</p>
+                </ul>
+            </div>
         </div>
     );
 }
