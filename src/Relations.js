4f598a5b8b7e4f03a29e6c32dcfdb0939866dec7
Griffin Sanford <griffin.sanford@gmail.com>
2021-06-08 11:55:10 -0400

    photos

--- src/Relations.js
+++ src/Relations.js
@@ -1,4 +1,8 @@
-import './Relations.css'
+import './Relations.css';
+import StaffAgenda from './StaffAgenda.png';
+import floorHockey from './FloorHockey.jpeg';
+import jays from './Jays.jpeg';
+import thomas from './thomas.jpeg';
 import React from 'react';
 
 
@@ -12,8 +16,9 @@ const Relations = () => {
             <div className='container'>
                 <ul>
                 <li className="dot">Development and Ongoing Implementation of MacLachlan's New Teacher Orientation Days</li>
+                <img className="Staff" src={StaffAgenda} alt="Staff" />
                 <p className='par'>Building relationships with staff is key and begins immediately.  I developed the MacLachlan New Teacher Orientation Program and continue to be a part of the team to deliver the workshops over two days in August.</p>
-                <li className="dot">Encouraging Professional Development</li>
+                <li className="dot"> Encouraging Professional Development</li>
                 <p className='par'>I present a budget to the Board for staff PD and encourage all staff to participate in continued PD to encourage new practices that match their interests and the school's goals.  Some of these have included PYP Workshops, Levels 1, 2 & 3, Cohort 21, Harvard's Project Zero and CIS Workshops.</p>
                 <li className="dot">Meetings & Conferences</li>
                 <p className='par'>I meet with teachers in several different ways, staff, divisional, grade and committee meetings are scheduled throughout the school year and each of those schedules are set in August prior to the start of that school year.  Individual meetings are made as needed and my door is always open.</p>
@@ -25,15 +30,14 @@ const Relations = () => {
                 <ul>
                 <li className="dot">Participating in School Events and School Life</li>
                 <p className='par'>I do not lead from my office.  I lead by example.  I start every day with morning door duty and greet every student by name as they enter the building.  I also end the day with traffic duty to say good-bye to students as they leave for the day.  I am highly visible in school events including Galas, Fun Fairs, Spirit Days and Student Staff sports.</p>
+                <img className="Hockey" src={floorHockey} alt="FloorHockey" />
+                <img className="Jays" src={jays} alt="Bluejays Spirit day" />
+                <img className="Thomas" src={thomas} alt="Thomas outfit" />
+                
+                
                 <li className="dot">Continued Love of Learning</li>
                 <p className='dot'>Learning never ends and I continue to expand my knowledge.</p>
-                <p className='par'>AQ courses</p>
-                <p className='par'>IB Workshops/Conferences</p>
-                <p className='par'>CIS Workshops</p>
-                <p className='par'>Coaching 2019</p>
-                <p className='par'>Unconference 2018</p>
-
-                
+                <p className='par'>AQ courses <br />IB Workshops/Conferences <br />CIS Workshops <br />Coaching 2019 <br />Unconference 2018</p>
                 </ul>
             </div>
             
